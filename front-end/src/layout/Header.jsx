6227306ec274faaa5f44562f41ad35303d3b3b86
ElodieCara <102527658+ElodieCara@users.noreply.github.com>
2024-01-22 11:12:29 +0100

    Ajout Nav-2nd

--- front-end/src/layout/Header.jsx
+++ front-end/src/layout/Header.jsx
@@ -1,12 +1,17 @@
 import { FaFacebook, FaPinterest } from "react-icons/fa";
 import { FaXTwitter, FaMagnifyingGlass } from "react-icons/fa6";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
 import Logo from "../assets/Tissatout.png";
 
 function Header() {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
   return (
     <header>
-      <div className="header__content">
-        <div className="header__content__social">
+      {/*-- #masthead --*/}
+      <div className="header__masthead">
+        <div className="header__masthead__social">
           <a href="">
             <FaFacebook size={30} />
           </a>
@@ -17,13 +22,13 @@ function Header() {
             <FaPinterest size={30} />
           </a>
         </div>
-        <div className="header__content__nav-main">
-          <div className="header__content__nav-main__logo">
+        <nav className="header__masthead__nav-main">
+          <div className="header__masthead__nav-main__logo">
             <img src={Logo} style={{ width: "180px" }} alt="Logo Tissatout" />
           </div>
-          <h1 className="header__content__nav-main__title">Tissatout</h1>
+          <h1 className="header__masthead__nav-main__title">Tissatout</h1>
           <form
-            className="header__content__nav-main__input"
+            className="header__masthead__nav-main__input"
             action="#"
             method="get"
           >
@@ -34,7 +39,46 @@ function Header() {
               </span>
             </button>
           </form>
-        </div>
+        </nav>
+      </div>
+      {/*-- #sub-masthead --*/}
+      <div className="header__sub-masthead">
+        <nav className="header__sub-masthead__nav">
+          <div className="header__sub-masthead__nav__container">
+            <ul className="header__sub-masthead__nav__container__navbar">
+              <li className="header__sub-masthead__nav__container__navbar-menu">
+                <NavLink exact="true" to="#" onClick={handleClick}>
+                  Title
+                </NavLink>
+              </li>
+              <li className="header__sub-masthead__nav__container__navbar-menu">
+                <NavLink exact="true" to="#" onClick={handleClick}>
+                  Title
+                </NavLink>
+              </li>
+              <li className="header__sub-masthead__nav__container__navbar-menu">
+                <NavLink exact="true" to="#" onClick={handleClick}>
+                  Title
+                </NavLink>
+              </li>
+              <li className="header__sub-masthead__nav__container__navbar-menu">
+                <NavLink exact="true" to="#" onClick={handleClick}>
+                  Title
+                </NavLink>
+              </li>
+              <li className="header__sub-masthead__nav__container__navbar-menu">
+                <NavLink exact="true" to="#" onClick={handleClick}>
+                  Title
+                </NavLink>
+              </li>
+              <li className="header__sub-masthead__nav__container__navbar-menu">
+                <NavLink exact="true" to="#" onClick={handleClick}>
+                  Title
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </div>
     </header>
   );
